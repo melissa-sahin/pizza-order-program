ec84997d7b15bafe945154987ab89bd22c07037b
Melisa Şahin <melisasahin3507@gmail.com>
2026-07-16 18:06:15 +0300

    First python

---
+++ pizza.py
@@ -0,0 +1,5 @@
+#Pizza Maker
+food = input("What would you like?")
+price = float(input("What price?"))
+toppings = input("What would you like?")
+print(f"Your food is {food} with {toppings} it's {price}" )
